314a6a2ca646567552d5a9c199895153079fbdea
JAYDEN1348 <1071427@hr.nl>
2025-06-13 13:05:32 +0200

    removed changes

--- client/Program.cs
+++ client/Program.cs
@@ -165,11 +165,11 @@ class ClientUDP
                 Message newMsg = decrypt(buffer, receivedMessage);
                 print(newMsg);
 
-                if (newMsg.MsgType == MessageType.DNSLookupReply)
+                if (newMsg.MsgType == MessageType.DNSLookupReply || newMsg.MsgType == MessageType.Error)
                 {
                     if (newMsg.MsgId != dnsLookup.MsgId)
                     {
-                        // Console.WriteLine($"The ID of the received message ({newMsg.MsgId}) does not match the expected ID ({dnsLookup.MsgId})!");
+                        Console.WriteLine($"The ID of the received message ({newMsg.MsgId}) does not match the expected ID ({dnsLookup.MsgId})!");
                         return;
                     }
 
@@ -183,9 +183,9 @@ class ClientUDP
                     byte[] acknowledgeMessage = encrypt(acknowledge);
                     sock.SendTo(acknowledgeMessage, acknowledgeMessage.Length, SocketFlags.None, ServerEndpoint);
                 }
-                else if (newMsg.MsgType == MessageType.Error)
-                {
-                }
+                // else if (newMsg.MsgType == MessageType.Error)
+                // {
+                // }
                 else
                 {
                     Console.WriteLine("Unexpected message type received!");
